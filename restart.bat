9a75e04de132f9375d4073df37e8d304b9ecb14d
Kozlov Sergey <kozlovsv78@gmail.com>
2024-02-21 19:15:25 +0500

    Исменен механизм билдинга. Доработано описание проекта

--- restart.bat
+++ restart.bat
@@ -1,4 +1,4 @@
 docker-compose down
-docker-compose up -d --build
+docker-compose up -d
 docker-compose logs server
 PAUSE
--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
-docker-compose up -d --build
+docker-compose up -d
 docker-compose logs server
 PAUSE
